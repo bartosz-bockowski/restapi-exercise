28b3adf83da0e12c51cac3bf39b7baef33e266e0
Bartek Boćkowski <bboc12357@gmail.com>
2023-12-09 17:41:14 +0100

    change handlers

--- src/main/java/com/example/restapi/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/restapi/config/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.restapi.config;
 
+import com.example.restapi.exception.*;
 import com.example.restapi.model.ApiError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,41 @@ import java.util.Optional;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    //invalid data
+    @ExceptionHandler({
+            UserWithThisUsernameAlreadyExistsException.class,
+            UserWithThisPeselAlreadyExistsException.class,
+            UserWithThisUsernameAndPeselAlreadyExistsException.class,
+            AppointmentCompletionTooEarlyException.class
+    })
+    public ResponseEntity<ApiError> handleInvalidDataException(RuntimeException exception) {
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+
+    //not found
+    @ExceptionHandler({
+            UserNotFoundException.class,
+            DoctorNotFoundException.class,
+            PatientNotFoundException.class,
+            AppointmentNotFoundException.class
+    })
+    public ResponseEntity<ApiError> handleNotFoundException(RuntimeException exception) {
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+
+    //access denied
+    @ExceptionHandler({
+            AccessDeniedException.class
+    })
+    public ResponseEntity<ApiError> handleAccessDeniedException(RuntimeException exception) {
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return new ResponseEntity<>(apiError, HttpStatus.FORBIDDEN);
+    }
+
+    //validation
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException e) {
         List<String> errors = e.getBindingResult().getFieldErrors().stream()
@@ -25,20 +61,11 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(getErrorsMap(errors), HttpStatus.BAD_REQUEST);
     }
 
-//    @ExceptionHandler(UserWithThisPeselAlreadyExistsException.class)
-//    public ResponseEntity<String> handlePeselTaken(UserWithThisPeselAlreadyExistsException e){
-//        return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
-//    }
-
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ApiError> handleInvalidDataException(RuntimeException exception) {
-        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, exception.getMessage());
-        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
-    }
-
+    //validation utils
     private Map<String, List<String>> getErrorsMap(List<String> errors) {
         Map<String, List<String>> errorResponse = new HashMap<>();
         errorResponse.put("errors", errors);
         return errorResponse;
     }
+
 }
